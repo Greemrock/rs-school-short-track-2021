3e64c3fb7d38764174ff37d34233138aa9425c39
HOME-PC\Andrey <greemrock911@gmail.com>
2021-04-12 23:22:17 +0300

    feat: task 11 complete

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,29 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  // throw new Error('Not implemented');
+  if (str.length === 1) {
+    return str;
+  }
+
+  const res = [];
+  let count = 1;
+  let prev = str[0];
+
+  for (let idx = 1; idx < str.length + 1; idx++) {
+    const cur = str[idx];
+
+    if (cur === prev) {
+      count += 1;
+    } else {
+      res.push(count === 1 ? prev : `${count}${prev}`);
+      prev = cur;
+      count = 1;
+    }
+  }
+
+  return res.join('');
 }
 
 module.exports = encodeLine;
